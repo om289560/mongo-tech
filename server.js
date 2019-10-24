f64dd84e25b0101ad5fcebd84575d65e26a82ffc
Oscar Miranda <omiranda0058@gmail.com>
2019-10-23 19:04:28 -0500

    added to files

---
+++ server.js
@@ -0,0 +1,43 @@
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+var express = require("express");
+var app = express();
+
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
+var exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+//mongoose.connect("mongodb://localhost/scraped_news");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/Mongo-Tech";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
+
+var routes = require("./controller/controller.js");
+app.use("/", routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
